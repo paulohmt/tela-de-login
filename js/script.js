fbdcc7147d97bb53a73d1dfb4c8daa826917fa5a
Pedro Henrique <80182753+Peedrohenrique@users.noreply.github.com>
2021-10-31 14:31:05 -0300

    alterações

--- js/script.js
+++ js/script.js
@@ -1,12 +1,38 @@
-function Entrar(){
-    var email = document.getElementById("nome").value;
-    var senha = document.getElementById("senha").value;
+var usuarios = [
+    { "login": "pedro", "senha": "34970994" },
+    { "login": "paulo", "senha": "112233" },
+    { "login": "sarah", "senha": "teamopedro" },
+];
 
-    if(email.value === 'paulo' || senha.value === '112233'){
-        localStorage("acesso", true);
-        alert("Usuário autenticado!")
-        window.location.href = "pagina1.html"
-    }else{
-        alert("Usuário ou senha inválidos!")
+function Entrar(){
+    var usuario = document.getElementsByName('username')[0].value.toLowerCase();
+    var senha = document.getElementsByName('password')[0].value;
+
+    for (var u in usuarios) {
+        var us = usuarios[u];
+        if (us.login === usuario && us.senha === senha) {
+            localStorage.setItem("acesso", true);
+            alert("usuário autenticado!")
+            window.location = ""
+            return true;
+        }
     }
 }
+
+
+
+
+/*function Entrar(){
+    const email = document.getElementById('nome').value;
+    const senha = document.getElementById('senha').value;
+
+    if(email.value === 'paulo' && senha.value === 'admin'){
+        localStorage("acesso", true);
+        alert("Usuário autenticado!")
+        window.location.href = "https://www.google.com/"
+    }else{
+        alert("Usuário ou senha inválidos!")
+        
+    }
+}
+*/
